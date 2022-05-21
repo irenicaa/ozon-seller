0c66d782df11d090397077eedf4a59b88ca69bb3
irenicaa <root@irenica.pro>
2022-05-21 23:07:56 +0300

    Add the GetProductInfoStocksResponseResultWrapper class

--- stocks.py
+++ stocks.py
@@ -3,6 +3,8 @@ from typing import List
 
 from dataclasses_json import dataclass_json
 
+# Request
+
 @dataclass_json
 @dataclass
 class ProductFilter:
@@ -16,3 +18,31 @@ class PaginatedProductFilter:
     filter: ProductFilter
     last_id: str
     limit: int
+
+# Response
+
+@dataclass_json
+@dataclass
+class GetProductInfoStocksResponseStock:
+    present: int
+    reserved: int
+    type: str
+
+@dataclass_json
+@dataclass
+class GetProductInfoStocksResponseItem:
+    offer_id: str
+    product_id: int
+    stocks: GetProductInfoStocksResponseStock
+
+@dataclass_json
+@dataclass
+class GetProductInfoStocksResponseResult:
+    items: List[GetProductInfoStocksResponseItem]
+    last_id: str
+    total: int
+
+@dataclass_json
+@dataclass
+class GetProductInfoStocksResponseResultWrapper:
+    result: GetProductInfoStocksResponseResult
