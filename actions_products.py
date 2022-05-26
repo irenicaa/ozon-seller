c5ccdeb57f611cef92b5e2794896bf705613a5fa
irenicaa <root@irenica.pro>
2022-05-27 00:14:04 +0300

    Add the GetSellerProductResponseResultWrapper class

--- actions_products.py
+++ actions_products.py
@@ -13,3 +13,28 @@ class ActionProducts:
     action_id: float
     limit: float
     offset: float
+
+# Response
+
+@dataclass_json
+@dataclass
+class GetSellerProductResponseProducts:
+    id: int
+    price: float
+    action_price: float
+    max_action_price: float
+    add_mode: str
+    min_stock: float
+    stock: float
+
+
+@dataclass_json
+@dataclass
+class GetSellerProductResponseResult:
+    products: list[GetSellerProductResponseProducts]
+    total: int
+
+@dataclass_json
+@dataclass
+class GetSellerProductResponseResultWrapper:
+    result: GetSellerProductResponseResult
