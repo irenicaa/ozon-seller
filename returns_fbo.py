1151a910568b2e2457e09996a2be85dee5fc9b0a
irenicaa <root@irenica.pro>
2022-05-27 18:46:55 +0300

    Add the ReturnsCompanyFboResponseWrapper class

--- returns_fbo.py
+++ returns_fbo.py
@@ -1,8 +1,9 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Optional
 import datetime
 
-from dataclasses_json import dataclass_json
+from dataclasses_json import dataclass_json, config
+from marshmallow import fields
 
 import request_api
 import credentials
@@ -21,3 +22,36 @@ class GetReturnsCompanyFboRequest:
     filter: GetReturnsCompanyFboRequestFilter
     offset: int
     limit: int
+
+# Response
+
+@dataclass_json
+@dataclass
+class ReturnsCompanyFboResponse:
+    accepted_from_customer_moment: datetime.datetime = field(
+        metadata=config(
+            decoder=datetime.datetime.fromisoformat,
+            mm_field=fields.DateTime(format='iso'),
+        ),
+    )
+    company_id: int
+    current_place_name: str
+    dst_place_name: str
+    id: int
+    is_opened: bool
+    posting_number: str
+    return_reason_name: str
+    returned_to_ozon_moment: datetime.datetime = field(
+        metadata=config(
+            decoder=datetime.datetime.fromisoformat,
+            mm_field=fields.DateTime(format='iso'),
+        ),
+    )
+    sku: int
+    status_name: str
+
+@dataclass_json
+@dataclass
+class ReturnsCompanyFboResponseWrapper:
+    count: int
+    returns: list[ReturnsCompanyFboResponse]
