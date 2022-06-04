e73e9504169f263ab132b778f428637426b2d0d4
irenicaa <root@irenica.pro>
2022-06-04 04:22:56 +0300

    Add the /v3/posting/fbs/get endpoint

--- main.py
+++ main.py
@@ -4,22 +4,14 @@ import datetime
 import dotenv
 
 import credentials
-import posting_fbs_list
+import posting_fbs_get
 
 if __name__ == '__main__':
     dotenv.load_dotenv()
 
-    data = posting_fbs_list.PaginatedGetPostingFBSListFilter(
-        filter=posting_fbs_list.GetPostingFBSListFilter(
-            since=datetime.datetime(2021, 11, 1),
-            to=datetime.datetime(2023, 11, 1),
-            status='awaiting_packaging',
-        ),
-        limit=2,
-        offset=0,
-    )
+    data = posting_fbs_get.PostingFBSData(posting_number='')
     print(data.to_json())
 
     ozon_credentials = credentials.Credentials(os.getenv('OZON_CLIENT_ID'), os.getenv('OZON_API_KEY'))
-    for list in posting_fbs_list.get_posting_fbs_list_iterative(ozon_credentials, data):
-        print(list)
+    list = posting_fbs_get.get_posting_fbs_data(ozon_credentials, data)
+    print(list)
---
+++ posting_fbs_get.py
@@ -0,0 +1,288 @@
+from dataclasses import dataclass, field
+from typing import Optional, Generator
+import datetime
+
+from dataclasses_json import dataclass_json, Undefined, config, CatchAll
+from marshmallow import fields
+
+import request_api
+import credentials
+import returns_fbs
+
+def parse_datetime(value):
+    if value is None:
+        return None
+    elif isinstance(value, str):
+        return datetime.datetime.fromisoformat(value)
+    elif isinstance(value, datetime.datetime):
+        return value
+    else:
+        raise RuntimeError('unsopported time for a datetime field')
+
+# Request
+
+@dataclass_json
+@dataclass
+class PostingAdditionalFields:
+    analytics_data: Optional[bool] = False
+    barcodes: Optional[bool] = False
+    financial_data: Optional[bool] = False
+    translit: Optional[bool] = False
+
+@dataclass_json
+@dataclass
+class PostingFBSData:
+    posting_number: str
+    with_: Optional[PostingAdditionalFields] = field(
+        default=None,
+        metadata=config(field_name="with"),
+    )
+
+
+# Response
+
+@dataclass_json
+@dataclass
+class GetPostinDataFBSResponseRequirements:
+    products_requiring_gtd: Optional[list[int]]
+    products_requiring_country: Optional[list[int]]
+    products_requiring_mandatory_mark: Optional[list[int]]
+    products_requiring_rnpt: Optional[list[int]]
+
+@dataclass_json
+@dataclass
+class GetPostinDataFBSResponseDimensions:
+    height: str
+    length: str
+    weight: str
+    width: str
+
+@dataclass_json
+@dataclass
+class GetPostinDataFBSResponseProduct:
+    dimensions: GetPostinDataFBSResponseDimensions
+    mandatory_mark: list[str]
+    name: str
+    offer_id: str
+    price: str
+    quantity: int
+    sku: int
+    currency_code: str
+
+@dataclass_json
+@dataclass
+class  GetPostinDataFBSResponseProductExemplarInfo:
+    mandatory_mark: str
+    gtd: str
+    is_gtd_absent: bool
+    rnpt: str
+    is_rnpt_absent: bool
+
+@dataclass_json
+@dataclass
+class  GetPostinDataFBSResponseProducts:
+    exemplars: list[GetPostinDataFBSResponseProductExemplarInfo]
+    sku: int
+
+@dataclass_json
+@dataclass
+class GetPostinDataFBSResponseProductExemplars:
+    products: list[GetPostinDataFBSResponseProducts]
+
+@dataclass_json
+@dataclass
+class GetPostinDataFBSResponseFinancialPicking:
+    amount: float
+    moment: datetime.datetime = field(
+        metadata=config(
+            decoder=datetime.datetime.fromisoformat,
+            mm_field=fields.DateTime(format='iso'),
+        ),
+    )
+    tag: str
+
+@dataclass_json
+@dataclass
+class GetPostinDataFBSResponseFinancialDataServices:
+    marketplace_service_item_deliv_to_customer: float
+    marketplace_service_item_direct_flow_trans: float
+    marketplace_service_item_dropoff_ff: float
+    marketplace_service_item_dropoff_pvz: float
+    marketplace_service_item_dropoff_sc: float
+    marketplace_service_item_fulfillment: float
+    marketplace_service_item_pickup: float
+    marketplace_service_item_return_after_deliv_to_customer: float
+    marketplace_service_item_return_flow_trans: float
+    marketplace_service_item_return_not_deliv_to_customer: float
+    marketplace_service_item_return_part_goods_customer: float
+
+@dataclass_json
+@dataclass
+class GetPostinDataFBSResponseDataProduct:
+    actions: list[str]
+    client_price: str
+    commission_amount: float
+    commission_percent: int
+    item_services: GetPostinDataFBSResponseFinancialDataServices
+    old_price: float
+    payout: float
+    picking: GetPostinDataFBSResponseFinancialPicking
+    price: float
+    product_id: int
+    quantity: int
+    total_discount_percent: float
+    total_discount_value: float
+
+@dataclass_json
+@dataclass
+class GetPostinDataFBSResponseFinancialData:
+    posting_services: GetPostinDataFBSResponseFinancialDataServices
+    products: list[GetPostinDataFBSResponseDataProduct]
+
+@dataclass_json
+@dataclass
+class GetPostinDataFBSResponseDeliveryMethod:
+    id: int
+    name: str
+    tpl_provider: str
+    tpl_provider_id: int
+    warehouse: str
+    warehouse_id: int
+
+@dataclass_json
+@dataclass
+class GetPostinDataFBSResponseAddress:
+    address_tail: str
+    city: str
+    comment: str
+    country: str
+    district: str
+    latitude: float
+    longitude: float
+    provider_pvz_code: str
+    pvz_code: int
+    region: str
+    zip_code: str
+
+@dataclass_json
+@dataclass
+class GetPostinDataFBSResponseCustomer:
+    address: GetPostinDataFBSResponseAddress
+    customer_email: str
+    customer_id: int
+    name: str
+    phone: str
+
+@dataclass_json
+@dataclass
+class GetPostinDataFBSResponseCourier:
+    car_model: str
+    car_number: str
+    name: str
+    phone: str
+
+@dataclass_json
+@dataclass
+class GetPostinDataFBSResponseCancellation:
+    affect_cancellation_rating: bool
+    cancel_reason: str
+    cancel_reason_id: int
+    cancellation_initiator: str
+    cancellation_type: str
+    cancelled_after_ship: bool
+
+@dataclass_json
+@dataclass
+class GetPostinDataFBSResponseBarcodes:
+    lower_barcode: str
+    upper_barcode: str
+
+@dataclass_json
+@dataclass
+class GetPostinDataFBStResponseAnalyticsData:
+    city: str
+    delivery_date_begin: Optional[datetime.datetime] = field(
+        metadata=config(
+            decoder=parse_datetime,
+            mm_field=fields.DateTime(format='iso', allow_none=True),
+        ),
+    )
+    is_premium: bool
+    payment_type_group_name: str
+    region: str
+    tpl_provider: str
+    tpl_provider_id: int
+    warehouse: str
+    warehouse_id: int
+
+@dataclass_json
+@dataclass
+class GetPostinDataFBSResponseAddressee:
+    name: str
+    phone: str
+
+@dataclass_json
+@dataclass
+class GetPostinDataFBSResponseAdditionalDataItem:
+    key: str
+    value: str
+
+@dataclass_json
+@dataclass
+class GetPostinDataFBSResponseResult:
+    additional_data: Optional[list[GetPostinDataFBSResponseAdditionalDataItem]]
+    addressee: Optional[GetPostinDataFBSResponseAddressee]
+    analytics_data: Optional[GetPostinDataFBStResponseAnalyticsData]
+    barcodes: Optional[GetPostinDataFBSResponseBarcodes]
+    cancellation: Optional[GetPostinDataFBSResponseCancellation]
+    courier: Optional[GetPostinDataFBSResponseCourier]
+    customer: Optional[GetPostinDataFBSResponseCustomer]
+    delivering_date: Optional[datetime.datetime] = field(
+        metadata=config(
+            decoder=parse_datetime,
+            mm_field=fields.DateTime(format='iso', allow_none=True),
+        ),
+    )
+    delivery_method: Optional[GetPostinDataFBSResponseDeliveryMethod]
+    delivery_price: str
+    financial_data: Optional[GetPostinDataFBSResponseFinancialData]
+    in_process_at: Optional[datetime.datetime] = field(
+        metadata=config(
+            decoder=parse_datetime,
+            mm_field=fields.DateTime(format='iso', allow_none=True),
+        ),
+    )
+    is_express: bool
+    order_id: int
+    order_number: str
+    posting_number: str
+    product_exemplars: Optional[GetPostinDataFBSResponseProductExemplars]
+    products: list[GetPostinDataFBSResponseProduct]
+    provider_status	: str
+    requirements: Optional[GetPostinDataFBSResponseRequirements]
+    shipment_date: Optional[datetime.datetime] = field(
+        metadata=config(
+            decoder=parse_datetime,
+            mm_field=fields.DateTime(format='iso', allow_none=True),
+        ),
+    )
+    status: str
+    tpl_integration_type: str
+    tracking_number: str
+
+@dataclass_json
+@dataclass
+class GetPostinDataFBSResponseResultWrapper:
+    result: GetPostinDataFBSResponseResult
+
+def get_posting_fbs_data(
+    credentials: credentials.Credentials,
+    data: PostingFBSData,
+) -> GetPostinDataFBSResponseResultWrapper:
+    response = request_api.request_api_raw(
+        'POST',
+        '/v3/posting/fbs/get',
+        credentials,
+        data.to_json(),
+    )
+    return GetPostinDataFBSResponseResultWrapper.schema().loads(response)
