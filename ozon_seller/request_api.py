d442525bbc0b658853d7570d7ff0bcf91e464f9c
irenicaa <root@irenica.pro>
2022-07-05 22:32:56 +0300

    Remove the request_api function

--- ozon_seller/request_api.py
+++ ozon_seller/request_api.py
@@ -38,14 +38,3 @@ def request_api_content(
     )
     response.raise_for_status()
     return response.content
-
-
-def request_api(
-    method: str,
-    endpoint: str,
-    credentials: credentials.Credentials,
-    data: object,
-) -> object:
-    json_data = json.dumps(data)
-    response = request_api_raw(method, endpoint, credentials, json_data)
-    return json.loads(response)
