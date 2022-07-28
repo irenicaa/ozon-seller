42f7d8ee8a23027e2f089dffa0c8c785bf982474
irenicaa <root@irenica.pro>
2022-07-28 22:07:48 +0300

    Add the /v1/chat/send/message endpoint

--- ozon_seller/__main__.py
+++ ozon_seller/__main__.py
@@ -3,20 +3,21 @@ import os
 
 import dotenv
 
-from . import chat_start, credentials, request_api, stocks
+from . import char_send_message, credentials, request_api, stocks
 
 if __name__ == "__main__":
     dotenv.load_dotenv()
     try:
-        data = chat_start.ChatStartData(
-            posting_number=os.getenv('POSTING_NUMBER')
+        data = char_send_message.ChatMessageData(
+            chat_id=os.getenv('CHAT_ID'),
+            text="Hello World!"
         )
         print(data.to_json())
 
         ozon_credentials = credentials.Credentials(os.getenv('OZON_CLIENT_ID'), os.getenv('OZON_API_KEY'))
 
-        chat_id = chat_start.get_product_info_stocks(ozon_credentials, data)
-        print(chat_id)
+        result = char_send_message.send_message(ozon_credentials, data)
+        print(result)
 
     except request_api.HTTPError as error:
         print('ERROR', error)
---
+++ ozon_seller/chat_send_message.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from typing import Generator, Optional
+
+from dataclasses_json import Undefined, dataclass_json
+
+from . import credentials, request_api
+
+# Request
+
+
+@dataclass_json(undefined=Undefined.EXCLUDE)
+@dataclass
+class ChatMessageData:
+    chat_id: Optional[str] = None
+    text: Optional[str] = None
+
+
+# Response
+
+
+@dataclass_json(undefined=Undefined.EXCLUDE)
+@dataclass
+class GetChatStartResponseResult:
+    result: str
+
+
+def send_message(
+    credentials: credentials.Credentials,
+    data: ChatMessageData,
+) -> GetChatStartResponseResult:
+    return request_api.request_api_json(
+        "POST",
+        "/v1/chat/send/message",
+        credentials,
+        data,
+        response_cls=GetChatStartResponseResult,
+    )
