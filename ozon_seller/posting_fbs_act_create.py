d966f8abaddf5e6b6314410e657d3f8cafe7b427
irenicaa <root@irenica.pro>
2022-09-16 21:25:32 +0300

    Udate datetime fields in the `posting_fbs_act_create.py` file

--- ozon_seller/posting_fbs_act_create.py
+++ ozon_seller/posting_fbs_act_create.py
@@ -5,11 +5,7 @@ from typing import Generator, Optional
 from dataclasses_json import CatchAll, Undefined, config, dataclass_json
 from marshmallow import fields
 
-from .common import credentials, request_api
-
-
-def format_datetime(value):
-    return value.astimezone(datetime.timezone.utc).isoformat(timespec="microseconds")
+from .common import credentials, request_api, datetime_field
 
 
 # Request
@@ -20,10 +16,9 @@ def format_datetime(value):
 class PostingFSBDeliveryData:
     containers_count: Optional[int] = None
     delivery_method_id: Optional[int] = None
-    departure_date: Optional[datetime.datetime] = field(
-        metadata=config(encoder=format_datetime),
-        default=None,
-    )
+    departure_date: Optional[
+        datetime.datetime
+    ] = datetime_field.optional_datetime_field()
 
 
 # Response
