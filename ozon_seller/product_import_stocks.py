f86cb72a6c1284381927ead0a09325ce909f001a
irenica <root@irenica.pro>
2023-08-30 01:56:53 +0300

    Add the /v2/products/stocks endpoint

---
+++ ozon_seller/product_import_stocks.py
@@ -0,0 +1,62 @@
+from dataclasses import dataclass
+from typing import Generator, Optional
+
+from dataclasses_json import Undefined, dataclass_json
+
+from .common import credentials, request_api
+
+# Request
+
+
+@dataclass_json(undefined=Undefined.EXCLUDE)
+@dataclass
+class ProductsStocksList:
+    offer_id: str
+    product_id: int
+    stock: int
+    warehouse_id: int
+
+
+@dataclass_json(undefined=Undefined.EXCLUDE)
+@dataclass
+class ProductImportProductsStocks:
+    stocks: list[ProductsStocksList]
+
+
+# Response
+
+
+@dataclass_json(undefined=Undefined.EXCLUDE)
+@dataclass
+class ProductImportProductsStocksResponseError:
+    code: str
+    message: str
+
+
+@dataclass_json(undefined=Undefined.EXCLUDE)
+@dataclass
+class ProductsStocksResponseProcessResult:
+    errors: list[ProductImportProductsStocksResponseError]
+    offer_id: str
+    product_id: int
+    updated: bool
+    warehouse_id: int
+
+
+@dataclass_json(undefined=Undefined.EXCLUDE)
+@dataclass
+class ProductsStocksResponseProcessResultWrapper:
+    result: list[ProductsStocksResponseProcessResult]
+
+
+def set_stocks(
+    credentials: credentials.Credentials,
+    data: ProductImportProductsStocks,
+) -> ProductsStocksResponseProcessResultWrapper:
+    return request_api.request_api_json(
+        "POST",
+        "/v2/products/stocks",
+        credentials,
+        data,
+        response_cls=ProductsStocksResponseProcessResultWrapper,
+    )
