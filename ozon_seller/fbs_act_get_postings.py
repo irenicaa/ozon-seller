03be5376387e54e3c50553957ebe90bd8c15e055
irenica <root@irenica.pro>
2023-10-13 18:41:01 +0300

    Add the /v2/posting/fbs/act/get-postings endpoint

---
+++ ozon_seller/fbs_act_get_postings.py
@@ -0,0 +1,59 @@
+from dataclasses import dataclass
+from typing import Generator, Optional
+
+from dataclasses_json import Undefined, dataclass_json, DataClassJsonMixin
+
+from .common import credentials, request_api, datetime_field
+
+# Request
+
+
+@dataclass
+class PostingFBSActData(DataClassJsonMixin):
+    id: Optional[int] = None
+
+
+# Response
+
+
+@dataclass_json(undefined=Undefined.EXCLUDE)
+@dataclass
+class PostingFBSActDataResponseProducts:
+    name: str
+    offer_id: str
+    price: str
+    quantity: int
+    sku: int
+
+
+@dataclass_json(undefined=Undefined.EXCLUDE)
+@dataclass
+class PostingFBSActDataResponseResult:
+    id: int
+    multi_box_qty: int
+    posting_number: str
+    status: str
+    seller_error: str
+    updated_at: str
+    created_at: str
+    products: list[PostingFBSActDataResponseProducts]
+
+
+
+@dataclass_json(undefined=Undefined.EXCLUDE)
+@dataclass
+class PostingFBSActDataResponseResultWrapper:
+    result: list[PostingFBSActDataResponseResult]
+
+
+def get_posting_fbs_act_data(
+    credentials: credentials.Credentials,
+    data: PostingFBSActData,
+) -> PostingFBSActDataResponseResultWrapper:
+    return request_api.request_api_json(
+        "POST",
+        "/v2/posting/fbs/act/get-postings",
+        credentials,
+        data,
+        response_cls=PostingFBSActDataResponseResultWrapper,
+    )
