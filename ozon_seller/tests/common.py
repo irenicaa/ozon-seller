729b08c4a5e99051c6271d7c4fca9feb4a1ffb55
irenica <root@irenica.pro>
2025-07-06 07:30:43 +0500

    Support for classes in the `get_last_module()` and `get_qualified_name()` functions

--- ozon_seller/tests/common.py
+++ ozon_seller/tests/common.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+
 TEST_DATA_DIRECTORY = "test-data"
 
 
@@ -5,9 +8,13 @@ def get_full_qualified_name(obj: object) -> str:
     return f"{get_last_module(obj)}.{get_qualified_name(obj)}"
 
 
-def get_last_module(obj: object) -> str:
-    return obj.__module__.split(".")[-1]
+def get_last_module(obj: Union[object, type[object]]) -> str:
+    return _get_class(obj).__module__.split(".")[-1]
 
 
-def get_qualified_name(obj: object) -> str:
-    return obj.__class__.__qualname__
+def get_qualified_name(obj: Union[object, type[object]]) -> str:
+    return _get_class(obj).__qualname__
+
+
+def _get_class(obj: Union[object, type[object]]) -> type[object]:
+    return obj if callable(obj) or isinstance(obj, type) else obj.__class__
